18e3b5f0b8ee767156c066811e9d72ae668e14cd
Vue Js commit <elazzaouimouade@gmail.com>
2019-01-22 13:00:31 +0000

    store

--- src/store.js
+++ src/store.js
@@ -9,7 +9,29 @@ export default new Vuex.Store({
     NEW_PRICE: '',
     NEW_NAME: '',
     items: [
-        {id:'1', price: '435$', name: 'IPhone 4'}
-    ]
-    },
+        {id:'1', price: '435$', name: 'IPhone 4'},
+        {id:'2', price: '730$', name: 'IPhone 7+'},
+        {id:'3', price: '920$', name: 'IPhone 8S'}
+        ]
+  },
+  mutations: {
+    ADD_PRODUCT(state, payload) {
+        state.items.push({
+            id: payload.NEW_ID,
+            price: payload.NEW_PRICE,
+            name: payload.NEW_NAME,
+        })
+    }
+  }
 })
+
+/*
+*
+*  state.items.push({
+        id: state.NEW_ID,
+        price: state.NEW_PRICE,
+        name: state.NEW_NAME,
+      })
+*
+*
+*/
